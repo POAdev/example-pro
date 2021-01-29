0e015daba627e3190ad28f209e8092a90eeb5a5b
Favour Vivian Woka <vivianemma97@gmai.com>
2021-01-29 14:39:22 +0100

    install bootstrap on project, use Axios fetch api,

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
+import { useEffect, useState } from "react";
+import  Ideas from "./data/ideas.json"
+import Axios from "axios"
 
 function App() {
+
+  const [someData, setSomeDate] = useState("");
+
+  useEffect(()=> {
+    Axios.get("https://postgrest-api.mainnet.dandelion.link/tx_metadata?id=eq.2806").
+    then(data =>{
+      console.log(data)
+      setSomeDate(data.data[0].json);
+    })
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Workshop Done</h1>
+        <h1>{Ideas.title}</h1>
+        <p>{Ideas.homeTown}</p>
+        <div >
+          {Ideas.ideals.map( e => <p>
+            {e.text}
+          </p>)}
+        </div>
+
+        <h2>{someData}</h2>
       </header>
     </div>
   );
